b7d23e7fd6839252bca7a90efbebb741197345ee
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-03-11 08:18:55 +0530

    MySQL Pool Connection Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+const { getConnectionAsync, executeAsync } = require('./lib');
+
+
+function SqlClient(options) {
+  if (!options) {
+    throw new Error('Invalid options');
+  }
+
+  this._options = options;
+}
+
+// Create pool connection
+SqlClient.prototype.connect = function() {
+  this._client = mysql.createPool(this._options);
+};
+
+SqlClient.prototype.queryAsync = async function (query, values) {
+  const connection = await getConnectionAsync(this._client);
+
+  // TODO format response
+  return await executeAsync(connection, query, values);
+};
+
+module.exports = SqlClient;
